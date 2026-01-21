47f80556b1145bd77a59c0e01af9a74ddf5f91a7
Haarith Abdul <Haarithabdul@gmail.com>
2026-01-21 13:35:21 +0000

    Changed file reading for streamlit cloud

--- classFile.py
+++ classFile.py
@@ -1,12 +1,15 @@
 #DEPENDENCIES
-
+from pathlib import Path
 import pandas as pd
 
 
 class Data:
     def __init__(self, quarter, filesDict):
+        BASE_DIR = Path(__file__).parent
+        DATA_FILE = BASE_DIR / filesDict[quarter]
+
         #Reads excel file given and specific sheet name
-        self.rawData = pd.read_excel(filesDict[quarter], sheet_name="Group detail")
+        self.rawData = pd.read_excel(DATA_FILE, sheet_name="Group detail")
 
     #Converts given file into a dataframe and renames columns
     def convertToDataFrame(self):
