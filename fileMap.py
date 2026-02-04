f5981d0a0cee5cdd8bb95584e241e0a95c3fff6c
Haarith Abdul <Haarithabdul@gmail.com>
2026-02-04 15:07:56 +0000

    Debugging

--- fileMap.py
+++ fileMap.py
@@ -1,6 +1,8 @@
 #DEPENDENCIES
 from pathlib import Path
 
+base_dir = Path(__file__).parent
+
 quarters = [
         "Q1-2023", "Q2-2023", "Q3-2023", "Q4-2023/24",
         "Q1-2024", "Q2-2024", "Q3-2024", "Q4-2024/25",
@@ -8,19 +10,21 @@ quarters = [
     ]
 
 filesDict = {
-    quarters[0]: "Dummy Q1-2023.xlsx",
-    quarters[1]: "Dummy Q2-2023.xlsx",
-    quarters[2]: "Dummy Q3-2023.xlsx",
-    quarters[3]: "Dummy Q4-2023.xlsx",
-    quarters[4]: "Dummy Q1-2024.xlsx",
-    quarters[5]: "Dummy Q2-2024.xlsx",
-    quarters[6]: "Dummy Q3-2024.xlsx",
-    quarters[7]: "Dummy Q4-2024.xlsx",
-    quarters[8]: "Dummy Q1-2025.xlsx",
-    quarters[9]: "Dummy Q2-2025.xlsx",
-    quarters[10]: "Dummy Q3-2025.xlsx",
-    quarters[11]: "Dummy Q4-2025.xlsx"
+    quarters[0]: base_dir / "Dummy Q1-2023.xlsx",
+    quarters[1]: base_dir / "Dummy Q2-2023.xlsx",
+    quarters[2]: base_dir / "Dummy Q3-2023.xlsx",
+    quarters[3]: base_dir / "Dummy Q4-2023.xlsx",
+    quarters[4]: base_dir / "Dummy Q1-2024.xlsx",
+    quarters[5]: base_dir / "Dummy Q2-2024.xlsx",
+    quarters[6]: base_dir / "Dummy Q3-2024.xlsx",
+    quarters[7]: base_dir / "Dummy Q4-2024.xlsx",
+    quarters[8]: base_dir / "Dummy Q1-2025.xlsx",
+    quarters[9]: base_dir / "Dummy Q2-2025.xlsx",
+    quarters[10]: base_dir / "Dummy Q3-2025.xlsx",
+    quarters[11]: base_dir / "Dummy Q4-2025.xlsx"
     }
+
+#Code for looping through directory and building dictionary dynamically
 
 '''
 def createDict():
